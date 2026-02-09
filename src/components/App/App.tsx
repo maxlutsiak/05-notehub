7e46fc130e8cf155827a9693b6fe10929eba0e2c
Maksym Lutsiak <demon164320@gmail.com>
2026-02-09 18:01:24 +0100

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+
 import { useEffect, useState } from "react";
 import NoteList from "../NoteList/NoteList";
 import { fetchNotes } from "../../services/noteService";
--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { Note } from "../types/note";
-import { FetchNextPageOptions } from "@tanstack/react-query";
 
-const API_KEY = import.meta.env.VITE_NOTEHUB_TOKEN;
 
 const api = axios.create({
   baseURL: "https://notehub-public.goit.study/api",
@@ -11,7 +9,7 @@ const api = axios.create({
   },
 });
 
-interface FetchNotesResponse {
+export interface FetchNotesResponse {
   notes: Note[];
   totalPages: number;
 }
@@ -22,21 +20,71 @@ export interface FetchNotesParams {
   search?: string;
 }
 
+export interface CreateNoteRequest {
+  title: string;
+  content: string;
+  tag?: string;
+}
+
 export async function fetchNotes(
-  params: FetchNotesParams,
+  params: FetchNotesParams
 ): Promise<FetchNotesResponse> {
-  const response = await api.get("/notes", {
-    params,
-  });
+  const response = await api.get<FetchNotesResponse>("/notes", { params });
   return response.data;
 }
 
-export async function createNote(newTask: Note) {
-  const response = await api.post<Note>("/notes", newTask);
+export async function createNote(
+  data: CreateNoteRequest
+): Promise<Note> {
+  const response = await api.post<Note>("/notes", data);
   return response.data;
 }
 
-export async function deleteNote(id: string) {
+export async function deleteNote(id: string): Promise<Note> {
   const response = await api.delete<Note>(`/notes/${id}`);
   return response.data;
 }
+
+
+// import axios from "axios";
+// import { Note } from "../types/note";
+// import { FetchNextPageOptions } from "@tanstack/react-query";
+
+// const API_KEY = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+// const api = axios.create({
+//   baseURL: "https://notehub-public.goit.study/api",
+//   headers: {
+//     Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+//   },
+// });
+
+// interface FetchNotesResponse {
+//   notes: Note[];
+//   totalPages: number;
+// }
+
+// export interface FetchNotesParams {
+//   page: number;
+//   perPage: number;
+//   search?: string;
+// }
+
+// export async function fetchNotes(
+//   params: FetchNotesParams,
+// ): Promise<FetchNotesResponse> {
+//   const response = await api.get("/notes", {
+//     params,
+//   });
+//   return response.data;
+// }
+
+// export async function createNote(newTask: Note) {
+//   const response = await api.post<Note>("/notes", newTask);
+//   return response.data;
+// }
+
+// export async function deleteNote(id: string) {
+//   const response = await api.delete<Note>(`/notes/${id}`);
+//   return response.data;
+// }
